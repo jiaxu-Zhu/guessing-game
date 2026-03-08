61c64deb95ed446358cbc3e9d7f090f7ef77f0fa
jiaxu-Zhu <jiaxu-Zhu@users.noreply.github.com>
2026-03-08 15:49:33 +0000

    chore: 每分钟累积优化 - v1.0.27
    • 无操作

--- game.js
+++ game.js
@@ -362,7 +362,7 @@ class GuessingGame {
         // 版本信息
         this.initSounds();
         this.version = {
-            number: "v1.0.26",
+            number: "v1.0.27",
             date: "2026-03-08",
             changes: [
                 "✨ 初始版本发布",
